aa8ffaacd6a67eaf0999ac5ae67dc85304747b91
Chris Jackson <chris.jackson@mrc-bsu.cam.ac.uk>
2024-04-03 10:50:02 +0100

    msmhist method for multistate data visualisation

--- R/msmbayes-package.R
+++ R/msmbayes-package.R
@@ -1,21 +1,21 @@
-#' The 'msmbayes' package.
+#' The 'msmbayes' package for Bayesian multi-state modelling of intermittently-observed data.
 #'
 #' @description For an introduction to and overview of the `msmbayes` package, and full documentation, see
 #'
-#' \url{GITHUB TODO}.
+#' \url{http://chjackson.github.io/msmbayes}.
 #'
-#' For details of the methods, see TODO
-#' 
-#' @references Kalbfleisch and Lawless, Statn. 
+#' For more resources on multi-state modelling, see the [`msm` package](http://chjackson.github.io/msm) and its documentation.
 #' 
 #' @name msmbayes-package
-#' @importFrom stats delete.response na.omit reshape setNames terms
+#' @importFrom stats delete.response na.omit reshape setNames terms quantile runif
 #' @importFrom posterior as_draws as_draws_matrix rhat ess_bulk rvar ndraws rvar_sum "%**%" rdo rvar_sum draws_of merge_chains
 #' @importFrom cli cli_abort cli_warn qty cli_progress_bar cli_progress_update cli_progress_done
 #' @importFrom glue glue
-#' @importFrom rlang caller_env
+#' @importFrom rlang caller_env .data
 #' @importFrom magrittr "%>%"
-#' @importFrom dplyr mutate select filter relocate left_join n arrange group_by pull row_number full_join summarise matches
+#' @importFrom dplyr mutate select filter relocate left_join n arrange group_by ungroup pull row_number full_join summarise matches
 #' @importFrom tidyr pivot_longer
-#' 
+#' @importFrom utils head
+#'
+#' @md
 "_PACKAGE"
---
+++ R/msmhist.R
@@ -0,0 +1,204 @@
+#' Illustrate the empirical distribution of states against time
+#' in intermittently-observed multistate data
+#'
+#' This works similarly to a histogram.  The state observations are
+#' binned into time intervals with roughly equal numbers of
+#' observations.  Within each bin, the probability \eqn{p(s)} that an
+#' observation comes from each state \eqn{s} is estimated.
+#'
+#' @details
+#'
+#' If each subject has at most one observation in a bin, then \eqn{p(s)}
+#' is estimated as the proportion of observations in the bin that are
+#' of that state.
+#' 
+#' More generally, if an individual has more than one observation in
+#' the bin, \eqn{p(s)} is estimated as follows. For each observed
+#' individual \eqn{i} and each state \eqn{s}, we define a variable
+#' \eqn{p(i,s)} equal to the proportion of individual \eqn{i}'s
+#' observations that are of state \eqn{s}. For example, in a
+#' three-state model, where a person has two observations in a bin,
+#' and these are states 2 and 3, then \eqn{p(i,s) = 0, 0.5, 0.5} for
+#' states 1, 2 and 3 respectively.  The bin-specific estimate of
+#' \eqn{p(s)} is then the average of \eqn{p(i,s)} over individuals
+#' \eqn{s} who have at least one observation in that bin.
+#'
+#' The results are visualised as a stacked bar plot.  The individual
+#' observations of states are represented as points placed at random y
+#' positions within each state-specific bar.
+#'
+#' This is intended as an alternative to the "observed prevalences"
+#'   plot in the function `prevalence.msm` from the `msm` package,
+#'   with a clearer connection to the data.   It can be overlaid
+#'   with predictions of transition probabilities from a `msmbayes`
+#'   or `msm` model, to check the fit of the model.
+#'
+#' The method used by "observed prevalences" plots places a strong
+#'   assumption on the individual data, that individuals stay in the
+#'   same state between observations, or transition at the midpoint
+#'   between observations.
+#'
+#'   `msmhist` places no assumption on the individual data.  Instead
+#'   the assumption is placed on the data-generating process.  The
+#'   histogram-like visualisation assumes, essentially, that the
+#'   distribution of states is the same at all times within each bin.
+#'
+#' @inheritParams msmbayes
+#' 
+#' @param nbins Number of time intervals to bin the state observations
+#'   into.  The underlying distribution of states illustrated by the
+#'   plot will be assumed constant within each interval.
+#'
+#' @param absorbing Indices of any absorbing states.  Individuals are
+#'   assumed to stay in their absorbing state, and contribute one
+#'   observation to each bin after their absorption time.  By default,
+#'   no states are assumed to be absorbing.
+#'
+#' @param censtimes Vector of maximum intended follow-up times for the
+#'   people in the data who entered absorbing states.  This supposes
+#'   that had the person not entered the absorbing state, they would
+#'   not have been observed after this time.
+#'
+#' @return A \code{ggplot2} plot object.
+#'
+#' @md
+#' @export
+msmhist <- function(data, state, time, subject, nbins,
+                    absorbing=NULL, censtimes=NULL){
+  data$state <- data[[state]] <- as.factor(data[[state]])
+  data$time <- data[[time]]
+  data$subject <- data[[subject]]
+  bardata <- msmhist_bardata(data, nbins, absorbing, censtimes)
+  data$ypos <- msmhist_random_ypos(data, bardata)
+
+  ggplot2::ggplot(data=bardata,
+                  ggplot2::aes(xmin=.data$binstart, xmax=.data$binend,
+                               ymin=.data$cumpstart, ymax=.data$cumpend,
+                               fill=.data$state)) +
+    ggplot2::geom_rect(col="black",alpha=0.5) +
+    ggplot2::geom_point(data=data,
+                        ggplot2::aes(x=.data$time,
+                                     y=.data$ypos,
+                                     colour=.data$state),
+                        inherit.aes=FALSE, alpha=0.7) +
+    ggplot2::ylab("")
+}
+
+## TODO
+## manual bins? and/or nicer binning heuristic?
+## error checking
+## worked example with comparison against observed prevalence
+## from both msmbayes and msm
+## as needed.  Separate histograms per state 
+
+#' Estimate state occupation probabilities to be illustrated by a bar
+#' plot in \code{msmhist}
+#'
+#' @param data pre-cleaned with columns "time", "state", "subject"
+#'
+#' @inheritParams msmhist
+#' 
+#' @return Data frame with columns `binid`, `binlabel`, `state`,
+#'   `binstart`, `binend`, `props`, `cumpstart`, `cumpend`
+#'
+#' @md
+#' @noRd
+msmhist_bardata <- function(data, nbins, absorbing=NULL, censtimes=NULL){
+  timebin <- subject <- state <- pstate <- props <- cumpend <- binid <- binstart <- binend <- NULL # silence r cmd check
+  qs <- msmhist_bins(data$time, nbins)
+  nbins <- length(qs) - 1
+  nst <- length(unique(data$state))
+  data$timebin <- cut(data$time, qs, include.lowest = TRUE)
+  datp <- msmhist_expand_absorbing(data, qs, absorbing, censtimes)
+
+  ## Calculate p(i,s): prop of each indivdual's observations in the bin
+  ## that are from each state 
+  pstate_subj_df <- datp %>%
+    group_by(timebin, subject) %>%
+    summarise(pstate = 1 / n()) %>%
+    ungroup()
+  ## Then estimate p(s) as the mean of p(i,s) over individuals observed in the bin
+  bardata <- datp %>%
+    left_join(pstate_subj_df, by=c("timebin","subject")) %>%
+    tidyr::complete(state, tidyr::nesting(subject, timebin),
+                    fill=list(pstate=0)) %>% # set p(i,s) = 0 if s not observed
+    group_by(timebin) %>%
+    mutate(n = length(unique(subject))) %>%
+    group_by(timebin, state) %>%
+    summarise(props = sum(pstate/n)) %>% 
+    group_by(timebin) %>%
+    mutate(cumpend = cumsum(props),
+           cumpstart = c(0, head(cumpend, -1)),
+           binid = match(timebin, levels(timebin)),
+           binstart = head(qs,-1)[binid],
+           binend = qs[-1][binid],
+           binmid = (binstart + binend)/2) %>%
+    ungroup()
+  attr(bardata,"qs") <- qs
+  bardata
+}
+
+#' Default procedure for obtaining bins given a vector of observation times
+#' 
+#' @noRd
+msmhist_bins <- function(time, nbins){
+  qs <- quantile(time, seq(0, 1, length=nbins+1))
+  unique(qs)
+}
+
+#' Expand msmhist data to include repeated observations of absorbing
+#' states after absorption
+#'
+#' For each person who died, place a dummy obs of each absorbing state
+#' in each bin following the person's time of absorption
+#'
+#' @param data Original dataset cleaned to have columns named time,
+#'   state and subject, plus a column for the time bin
+#'
+#' @param qs Time quantiles used for binning
+#'
+#' @return Data frame with columns timebin, state, subject
+#'
+#' @inheritParams msmhist
+#'
+#' @noRd
+msmhist_expand_absorbing <- function(data, qs, absorbing, censtimes){
+  datp <- data[,c("timebin","state","subject")]
+  nbins <- length(qs) - 1
+  for (i in absorbing){
+    abstimes <- data$time[data$state %in% i] # assuming only one abs obs per pt
+    absbin <- findInterval(abstimes, qs, all.inside = TRUE)
+    ## Exclude bins after bin containing censoring time, after which
+    ## people will not have been followed up had they survived.
+    if (!is.null(censtimes))
+      maxbin <- pmax(findInterval(censtimes, qs, all.inside = TRUE), absbin)
+    else maxbin <- nbins
+    bins_extra <- sequence(nvec = maxbin-absbin, from=absbin+1, by=1)
+    bins_extra <- factor(bins_extra, levels=1:nbins, labels=levels(data$timebin))
+    subj_extra <- data$subject[data$state %in% i] # assuming only one abs obs per pt
+    subj_extra <- rep(subj_extra, maxbin-absbin)
+    dat_extra <- data.frame(timebin = bins_extra,
+                            state = rep(i, length(bins_extra)),
+                            subject = subj_extra)
+    datp <- rbind(datp, dat_extra)
+  }
+  datp
+}
+
+#' Obtain y coordinate needed to plot individual state observations
+#' for the msmhist plot.
+#'
+#' @param bardata output of \code{\link{msmhist_bardata}}
+#'
+#' @noRd
+msmhist_random_ypos <- function(data, bardata){
+  data$binid <- findInterval(data$time, attr(bardata,"qs"), all.inside = TRUE)
+  data$bardataid <- match(paste(data$binid, data$state),
+                       paste(bardata$binid, bardata$state))
+  set.seed(1)
+  ypos <- runif(nrow(data),
+                bardata$cumpstart[data$bardataid],
+                bardata$cumpend[data$bardataid])
+  ypos
+}
+
